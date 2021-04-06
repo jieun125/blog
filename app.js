201a2c6f7ea69d19d0bc817e3faa3c63854d927c
jieun125 <j._.eun@daum.net>
2021-04-07 00:04:24 +0900

    fixed a bug

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ import globalRouter from "./routers/globalRouter"
 const app = express();
 
 app.set('view engine', "pug");
-app.use(cookieParser);
-app.use(bodyParser.json);
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
